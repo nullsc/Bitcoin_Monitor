3b1cb95b445690f733d64bed6fdd878eac53df0f
nullsc <53406164+nullsc@users.noreply.github.com>
2019-12-18 00:35:26 +0000

    Update Crypto_Logger.py

--- Crypto_Logger.py
+++ Crypto_Logger.py
@@ -3,7 +3,7 @@
 
 #To use: add a call to the coin you want
 #number is which part of json it uses: 0 is btc
-#TODO: may add feature to disable the print out, html logs, other notifications
+#TODO: may add feature to disable the print out(done), html logs, other notifications
 
 try:
     import requests, sys, json, time
@@ -11,22 +11,23 @@ try:
 except ImportError as e:
     print(e)
 
-print("Crypto currency price logger")
-print("github.com/nullsc/Bitcoin_Monitor/ \n")
+print("Crypto currency prices logger")
 
 apiUrl = 'https://api.coinmarketcap.com/v1/ticker/'
-btcalertPRICE = float('7569.1')
-TIME = 30 #time in minutes to poll
+btcalertPRICE = 7000.0
+TIME = 15 #time in minutes to poll
 logFile = "log.txt"
 
 
 class checker(object):
-    def __init__(self, name, alertPrice = 0):
+    '''Class to check the crypto API'''
+    def __init__(self, name, logging = True, alertPrice = 0):
         self.name = name #instance
-        self.coin = "null" #none
+        self.coin = "null"
         self.sym = "null"
         self.price = 0
         self.alertPrice = alertPrice #0 by default, zero will also deactivate it
+        self.logging = logging #on by default
 
     def banner(self):
         print("checking for " + self.name)
@@ -42,27 +43,32 @@ class checker(object):
         cryptoData = json.loads(priceRequest.text) #convert to dict
 
         self.coin = cryptoData[cIndex]['name']
-        self.price = float(cryptoData[cIndex]['price_usd'])
+        self.price = float(cryptoData[cIndex]['price_usd']) # 7569.17021495
         self.sym = cryptoData[cIndex]['symbol']
         print(self.coin)
         print(self.price)
         if(self.alertPrice > 0): #start logging
             if(self.price < self.alertPrice):
                 print("[ALERT]Price has dipped below {}".format(str(self.alertPrice)))
-                self.log("[ALERT]Price has dipped below {}".format(str(self.alertPrice))) #append the msg
+                if(self.logging == True):
+                    self.log("[ALERT]Price has dipped below {}".format(str(self.alertPrice))) #append the msg
             else:
-                self.log()
+                if(self.logging == True):
+                    self.log()
         else:
-            self.log()
+            if(self.logging == True):
+                self.log()
 
  
-btc = checker("btc", btcalertPRICE) #first arg is a name which may be used later, second argument is the alert price
-eth = checker("eth", 200)
+
+btc = checker("btc", True, btcalertPRICE)
+eth = checker("eth", True, 140)
+#btc.scrape(0) #number is which part of json it uses: 0 is btc
+
 
 while (1):
     btc.scrape(0)
     eth.scrape(1)
     time.sleep(TIME * 60)
-
 
 
