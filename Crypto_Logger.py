45954f2c8bab78dcb2094eeaf9afab3bfe8f8d89
nullsc <53406164+nullsc@users.noreply.github.com>
2019-12-31 23:14:29 +0000

    Update Crypto_Logger.py

--- Crypto_Logger.py
+++ Crypto_Logger.py
@@ -1,15 +1,15 @@
 #Basic project to monitor crypto currency prices
 #07/12/2019
-
-version = "v1.1"
+version = "v1.3"
 #To use: add a call to the coin you want
 #number is which part of json it uses: 0 is btc
 
 #TODO: may add feature to disable the print out, html logs, other notifications, add try statement to check internet connection
+# add main func
 
 #Issues: On the ticker directory I noticed that the coins are sorted by 'rank'. Although this is not an issue
 #currently it could become an issue if you were using the tool long term and the ranks changed.
-#A way to get around this is to use for example the /bitcoin folder and the index would be 0. although
+#A way to get around this is to use for example the /bitcoin folder and the index would be 0. Although
 #doing this you would have to a different url for each instance, you could use the first arg to do this
 
 try:
@@ -19,13 +19,15 @@ except ImportError as e:
     print(e)
 
 print("Crypto currency prices logger")
-print("https://github.com/nullsc/Bitcoin_Monitor \n")
+print("https://github.com/nullsc \n")
 
 apiUrl = 'https://api.coinmarketcap.com/v1/ticker/'
-btcalertPRICE = 7000.0 
+btcalertPRICE = 7000.0 #float('7569.1')
 TIME = 30 #time in minutes to poll
 logFile = "log.txt"
 printPrices = True #Will print the prices each time
+
+RETRYTIME = 5 #time in minutes to retry after a connection failure
 
 
 class checker(object):
@@ -53,18 +55,17 @@ class checker(object):
             priceRequest = requests.get(apiUrl)
         except requests.exceptions.Timeout: #if it timesout, sleep then retry
             print("Connection Timed Out, Retrying")
-            time.sleep(1 * 60)
+            time.sleep(RETRYTIME * 60)
             return self.scrape()
         except requests.exceptions.RequestException as e: #if there is an error, sleep then retry
             print("Connection Error, Retrying")
-            print(e)
-            time.sleep(1 * 60)
+            #print(e)
+            time.sleep(RETRYTIME * 60)
             return self.scrape()
 
         cryptoData = json.loads(priceRequest.text) #convert to dict
-
         self.coin = cryptoData[self.cIndex]['name']
-        self.price = float(cryptoData[self.cIndex]['price_usd'])
+        self.price = float(cryptoData[self.cIndex]['price_usd']) # 7569.17021495
         self.sym = cryptoData[self.cIndex]['symbol']
         if(printPrices):
             print(self.coin)
@@ -82,13 +83,14 @@ class checker(object):
             if(self.logging == True):
                 self.log()
 
+ 
 
 btc = checker("btc", 0, True, btcalertPRICE) #note, coin index, logging?, price to alert at
 eth = checker("eth", 1, True, 100)
 
 
 while (1):
-    btc.scrape()
+    btc.scrape()#start scraping
     eth.scrape()
     time.sleep(TIME * 60)
 
